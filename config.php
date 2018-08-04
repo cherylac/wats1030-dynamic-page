390c8646f23540042e26a9327b0453c3d2a287e6
Cheryl <cherylcmpbll@yahoo.com>
2018-08-04 22:30:48 +0000

    updated config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$API_KEY = 'Put your API key here'; // This will be your Flickr-issued API Key.
-$PAGE_TITLE = 'Kittens and Rainbows'; // This should be a user-friendly page title.
-$SEARCH_TAGS = 'kitten,rainbow'; // This must be a comma-separated list
+$API_KEY = '2153ae2ab1126a29bc700e9128f22754';
+$PAGE_TITLE = 'The World of Cherl';
+$SEARCH_TAGS = 'puppy,rainbow,rain,fun'; 
 
 ?>
